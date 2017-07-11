d245e49ddd99f8a1e4708fba88714ffd800e3359
Sherly Chan <sherlyc@gmail.com>
2017-07-11 20:42:45 +1200

    test 7 accepting input test passed

--- components.spec.js
+++ components.spec.js
@@ -46,4 +46,12 @@ describe('InputArea', () => {
             <button>Add</button>
         ])).to.equal(true);
     })
+
+    it('should accept input', () => {
+        const wrapper = mount(<InputArea/>);
+        const input = wrapper.find('input');
+        input.simulate('change', {target: { value: 'Resin' }});
+        expect(wrapper.state('text')).to.equal('Resin');
+        expect(input.prop('value')).to.equal('Resin');
+    })
 })
